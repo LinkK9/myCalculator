277fc0b1a12c7603789646c031643b57455054ab
LinK <linhgtx@gmail.com>
2020-12-11 00:26:17 +0700

    bo sung tinh nang

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ let expression = "";
 function pushText(char) {
   expression += char;
   screenView += char;
+  screenView = screenView.replace("**2", "²");
+  screenView = screenView.replace("**3", "³");
   screenView = screenView.replace("*", "×");
   screenView = screenView.replace("/", "÷");
   document.getElementById("calculator-screen").innerText = screenView;
@@ -15,4 +17,20 @@ function clearScreen() {
   document.getElementById("calculator-screen").innerText = "0";
   expression = "0";
   screenView = "";
+}
+function sqrt() {
+  expression = "Math.sqrt(" + expression + ")";
+  cal();
+}
+function giaiThua(num) {
+  let gt = 1;
+  for (let i = 1; i <= num; i++) {
+    gt *= i;
+  }
+  return gt;
+}
+
+function gtButton() {
+    expression = giaiThua(eval(expression));
+    cal();
 }
